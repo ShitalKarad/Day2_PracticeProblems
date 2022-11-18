81ee36f6b15a642ea0195c64d9a94757db2a3011
Shital <114717834+ShitalKarad@users.noreply.github.com>
2022-11-18 14:58:08 +0530

    Update ReverseNumberInForLop.java

--- src/day2_Practice_Problems/ReverseNumberInForLop.java
+++ src/day2_Practice_Problems/ReverseNumberInForLop.java
@@ -6,7 +6,6 @@ public class ReverseNumberInForLop {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a value");
-        System.out.println("Reverse numbers are ");
         int i = sc.nextInt();
         int rev=0;
         for(; i!=0;i=i/10){
@@ -14,6 +13,6 @@ public class ReverseNumberInForLop {
             rev = rev*10+a;
 
         }
-        System.out.println(rev + " ");
+        System.out.println("Rverse number is " + rev + " ");
     }
 }
